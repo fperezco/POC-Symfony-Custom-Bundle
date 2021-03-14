5b2a76f8b20d55613d3444df80ab7fa28205db8f
jonytrifulca <jonytrifulca@hotmail.com>
2021-03-14 17:00:37 +0100

    add symfony/yaml to composer and refine FunctionalTest.php

--- tests/FunctionalTest.php
+++ tests/FunctionalTest.php
@@ -5,19 +5,36 @@ namespace Paco\CustomPacoBundle\Tests;
 
 
 
+use Paco\CustomPacoBundle\PacoNumberGenerator;
+use Paco\CustomPacoBundle\PacoNumberGeneratorBundle;
 use PHPUnit\Framework\TestCase;
+use Symfony\Component\Config\Loader\LoaderInterface;
+use Symfony\Component\HttpKernel\Kernel;
 
 class FunctionalTest extends TestCase
 {
     public function testServiceWiring()
     {
+        //this 3 lines to boot a real symfony app
+        $kernel = new RealSymfonyAPPTestingKernel('test', true);
+        $kernel->boot();
+        $container = $kernel->getContainer();
+
+        //we test that there is a right wiring
+        $ipsum = $container->get('custom_paco_bundle.paco_number_generator');
+        $this->assertInstanceOf(PacoNumberGenerator::class, $ipsum);
+        $this->assertInternalType('string', $ipsum->getNumber());
     }
 }
 
-class KnpULoremIpsumTestingKernel extends Kernel
+class RealSymfonyAPPTestingKernel extends Kernel
 {
+    //we use to enable our bundle
     public function registerBundles()
     {
+        return [
+            new PacoNumberGeneratorBundle(),
+        ];
     }
     public function registerContainerConfiguration(LoaderInterface $loader)
     {
