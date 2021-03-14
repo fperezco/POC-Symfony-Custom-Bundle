9a4bcd57bba504d7955d742ed81511c3e7be35f4
jonytrifulca <jonytrifulca@hotmail.com>
2021-03-14 18:11:35 +0100

    refactor FunctionalTest.php to add the method to test bundle with configuration

--- tests/FunctionalTest.php
+++ tests/FunctionalTest.php
@@ -5,10 +5,12 @@ namespace Paco\CustomPacoBundle\Tests;
 
 
 
+use Paco\CustomPacoBundle\Interfaces\MeetingMessageProviderInterface;
 use Paco\CustomPacoBundle\PacoNumberGenerator;
 use Paco\CustomPacoBundle\PacoNumberGeneratorBundle;
 use PHPUnit\Framework\TestCase;
 use Symfony\Component\Config\Loader\LoaderInterface;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Kernel;
 
 class FunctionalTest extends TestCase
@@ -25,10 +27,48 @@ class FunctionalTest extends TestCase
         $this->assertInstanceOf(PacoNumberGenerator::class, $ipsum);
         $this->assertInternalType('string', $ipsum->getNumber());
     }
+
+    public function testServiceWiringWithConfiguration()
+    {
+        //init app with custom configuration
+        $kernel = new RealSymfonyAPPTestingKernel([
+                    'meeting_message_provider' => 'example_meeting_provider'
+                    ]);
+        $kernel->boot();
+        $container = $kernel->getContainer();
+
+        //test that the service injected is the correct service
+        $ipsum = $container->get('custom_paco_bundle.paco_number_generator');
+        $this->assertContains('fake', $ipsum->getNumber());
+    }
+
+
 }
 
 class RealSymfonyAPPTestingKernel extends Kernel
 {
+    private $bundleConfig;
+
+    /*
+        When you boot a kernel, it creates a tests/cache directory
+        that includes the cached container. The problem is that it's
+        using the same cache directory for both tests. Once the cache
+        directory is populated the first time, all future tests re-use
+        the same container from the first test, instead of building their
+        own.
+
+     */
+    public function getCacheDir()
+    {
+        return __DIR__.'/cache/'.spl_object_hash($this);
+    }
+
+    public function __construct(array $bundleConfig = [])
+    {
+        $this->bundleConfig = $bundleConfig;
+        parent::__construct('test', true);
+    }
+
     //we use to enable our bundle
     public function registerBundles()
     {
@@ -36,7 +76,25 @@ class RealSymfonyAPPTestingKernel extends Kernel
             new PacoNumberGeneratorBundle(),
         ];
     }
+
+    //this is the method that's responsible for parsing all the YAML files in the config/packages directory and the services.yaml file.
     public function registerContainerConfiguration(LoaderInterface $loader)
     {
+        //Instead of parsing YAML files, we can instead put all that logic into PHP with $loader->load()
+        //passing it a callback function with a ContainerBuilder argument.
+        // Inside of here, we can start registering services and passing bundle extension configuration
+        $loader->load(function(ContainerBuilder $container) {
+            $container->register('example_meeting_provider', ExampleOfMessageProvider::class);
+            $container->loadFromExtension('paco_number_generator', $this->bundleConfig);
+        });
+    }
+}
+
+
+class ExampleOfMessageProvider implements MeetingMessageProviderInterface
+{
+    public function getWordList(): array
+    {
+        return ['fake1test', 'fake2test'];
     }
 }
